17ee8180eecc96490d95ee733d9995aa0ce73dcf
McOtech <otienopaul36@gmail.com>
2023-12-11 17:15:54 +0300

    build: implement storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,66 @@
+// @ts-check
+import type { Preview } from '@storybook/react';
+import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtime';
+
+import * as NextImage from 'next/image';
+import '../src/app/globals.css';
+// import { AuthProvider } from '../state/auth/AuthContext';
+
+const BREAKPOINTS_INT = {
+  xs: 375,
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xl: 1536,
+};
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    console.log(val);
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
+// Allow Storybook to handle Next's <Image> component
+const OriginalNextImage = NextImage.default;
+
+// Object.defineProperty(NextImage, 'default', {
+//   configurable: true,
+//   value: (props) => <OriginalNextImage {...props} unoptimized />,
+// });
+
+// export const decorators = [
+//   (Story) => (
+//     <AuthProvider>
+//       <Story />
+//     </AuthProvider>
+//   ),
+// ];
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    viewport: { viewports: customViewports },
+    layout: 'fullscreen',
+    nextRouter: {
+      Provider: RouterContext.Provider,
+    },
+  },
+};
+
+export default preview;
